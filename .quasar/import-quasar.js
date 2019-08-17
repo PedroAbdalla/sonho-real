3ebb82ee97eda4be5e969d8e717bd464c0863409
Pedro Abdala <pedroabdalla@outlook.com>
2019-08-17 20:15:28 -0300

    subindo quasar

---
+++ .quasar/import-quasar.js
@@ -0,0 +1,19 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.conf.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+
+
+import Vue from 'vue'
+
+import {Quasar,QLayout,QHeader,QDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QItem,QItemSection,QItemLabel,Ripple,Notify} from 'quasar'
+
+
+Vue.use(Quasar, { config: {},components: {QLayout,QHeader,QDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QItem,QItemSection,QItemLabel},directives: {Ripple},plugins: {Notify} })
